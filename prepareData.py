a7e6cc7dc70857af966f7385b8689d8f7c784085
m-orlewski <72623801+m-orlewski@users.noreply.github.com>
2023-11-08 14:32:09 +0100

    Add files via upload

---
+++ prepareData.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+
+universityDataRawDf = pd.read_csv('./universities_data_raw.csv')
+universityRankingDf = pd.read_json('./universities_ranking.json')
+
+universityRankingDf = universityRankingDf[universityRankingDf['location'] == 'United States']
+universityRankingDf = universityRankingDf[['rank', 'name']]
+universityRankingDf = universityRankingDf.rename(columns={'rank': 'Ranking', 'name': 'Name'})
+
+universityDataDf = universityDataRawDf.merge(universityRankingDf, on='Name', how='left')
+
+print(universityDataDf.shape)
+
+universityDataDf['Ranking'] = universityDataDf['Ranking'].fillna('1501+')
+universityDataDf['Ranking'] = universityDataDf.apply(lambda x: x['Ranking'].replace('=', ''), axis=1)
+universityDataDf['Ranking display'] = np.where(universityDataDf['Ranking'] == '1501+', '1500+', 'top 1500')
+
+universityDataDf = universityDataDf[['Name', 'Ranking', 'Ranking display', 'Applicants total', 'Admissions total', 'Enrolled total', 'Tuition and fees, 2010-11', 
+           'Tuition and fees, 2011-12', 'Tuition and fees, 2012-13', 'Tuition and fees, 2013-14', 'Control of institution',
+           'Total price for in-state students living on campus 2013-14', 'Total price for out-of-state students living on campus 2013-14']]
+           
+print(universityDataDf.shape)
+print(universityDataDf['Ranking display'].unique())
+print(universityDataDf['Ranking display'].value_counts())
+universityDataDf.to_csv('./university_data.csv', index=False)
+
